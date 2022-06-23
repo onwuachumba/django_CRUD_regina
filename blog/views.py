f3acdaa92da3f5ccef559a480c9335d058ccbe9d
regina onwuachumba <ginamartinsr@gmail.com>
2022-06-23 09:54:46 +0100

    first commit

---
+++ blog/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+from msilib.schema import Upgrade
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+
+from .models import Post
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
